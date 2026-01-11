e3d0bcb633132046d7734afbb5bc50fa7eda7691
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-11 04:51:08 -0700

    added comments

--- basic_variable/src/main.rs
+++ basic_variable/src/main.rs
@@ -6,10 +6,10 @@ fn main() {
 
     println!("What city were you born in?");
 
-    let mut city: String = String::new();
-    io::stdin()
-        .read_line(&mut city)
-        .expect("Failed to read line");
+    let mut city: String = String::new(); // String type to hold user input
+    io::stdin() // Get standard input
+        .read_line(&mut city) // Read a line into the city variable
+        .expect("Failed to read line"); // Handle potential errors
 
     println!("What was the name of your first pet?");
 
@@ -18,9 +18,10 @@ fn main() {
         .read_line(&mut pet)
         .expect("Failed to read line");
 
-    println!("Your band name is {}{}", city.trim_end(), pet.trim_end());
+    println!("Your band name is {}{}", city.trim_end(), pet.trim_end()); // Concatenate and display the band name
+
 
     println!("Program finished. Press Enter to exit...");
-        let stdin = io::stdin();
-        let _ = stdin.lock().lines().next();
+        let stdin = io::stdin(); // Get standard input
+        let _ = stdin.lock().lines().next(); // Wait for user to press Enter
 }
