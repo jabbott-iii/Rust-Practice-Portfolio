b25ef188c0d6d8ecd34a5591363ce567a8b702e2
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-11 22:51:04 -0700

    completed basic_if_3

--- basic_if_3/src/main.rs
+++ basic_if_3/src/main.rs
@@ -1,9 +1,7 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
-}
-
-
-/* print('''
+    println!(r#"
 *******************************************************************************
           |                   |                  |                     |
  _________|________________.=""_;=.______________|_____________________|_______
@@ -24,34 +22,70 @@ ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 *******************************************************************************
-      ''')
-print("Welcome to Treasure Island!")
-print("Your mission is to find the treasure.")
+    "#);
 
-direction = input("Do you want to go left or right? ").lower()
-if direction == "left":
-    print("You travel west and encounter a giant river.")
-elif direction == "right":
-    print("The ground gives way to a giant sinkhole, unfortunately, you died.")
-else:
-    print("Please choose a direction.")
+    println!("Welcome to Treasure Island!");
+    println!("Your mission is to find the treasure.");
 
-if direction == "left":
-    swimming = input("Do you want to swim or wait?").lower()
-    if swimming == "swim":
-        print("You have been eaten by aligators.")
-    elif swimming == "wait":
-        print("A ferry has arrived to take you across the river safely.")
-    else:
-        print("Please choose to swim or wait.")
+    // First decision point
+    println!("Do you want to go left or right?");
+    let mut direction = String::new();
+    while direction.trim_end() != "left" && direction.trim_end() != "right" {
+        direction.clear(); // Clear previous input - must have this
+        std::io::stdin()
+            .read_line(&mut direction)
+            .expect("Failed to read line");
+        println!("Please choose left or right.");
+    }
 
-if direction == "left" and swimming == "wait":
-    door = input("You stumble upon three colorfull doors. The first door is in a meadow and the color yellow. The second door is red and near a volcano. The third door is blue and near a lake. Which door do you choose?").lower()
-    if door == "blue":
-        print("As you approach the door. The vegetation you are walking on sinks. You are in a marsh! You struggle to break free from the mud. Your efforts are in vain as the mud grips you from the waist down pulling you under with every muscle movement. You eventually are swallowed whole by the earth.")
-    elif door == "red":
-        print("You approach the door and you can feel an intense heat growing as you get cloer. You open the door and lava spills forth, dissolving your body within seconds.")
-    elif door == "yellow":
-        print("You open the door with excitment for what lies beyond. As the door open you gasp in disbelief, before you is a sea of gold for as far as the eye can see. You have found the treasure!")
-    else:
-        print("Please choose blue, red, or yellow") */
+    if direction.trim_end() == "right" {
+            println!("The ground gives way to a giant sinkhole, unfortunately, you died.");
+        }
+
+    // Continue the story for the "left" option
+    if direction.trim_end() == "left" {
+        println!("You travel west and encounter a giant river.");
+        println!("Do you want to swim or wait?");
+        let mut swimming = String::new();
+        while swimming.trim_end() != "swim" && swimming.trim_end() != "wait" {
+            swimming.clear(); // Clear previous input - must have this
+            std::io::stdin()
+                .read_line(&mut swimming)
+                .expect("Failed to read line");
+            println!("Please choose to swim or wait.");
+        }
+
+        if swimming.trim_end() == "swim" {
+             println!("You have been eaten by alligators.");
+        } 
+    
+        // Continue the story for the "wait" option
+        else if swimming.trim_end() == "wait" {
+            println!("A ferry has arrived to take you across the river safely.");
+            println!("You stumble upon three colorful doors. The first door is in a meadow and is yellow. The second door is red and near a volcano. The third door is blue and near a lake. Which door do you choose?");
+            let mut door = String::new();
+            while door.trim_end() != "blue" && door.trim_end() != "red" && door.trim_end() != "yellow" {
+                door.clear(); // Clear previous input - must have this
+                std::io::stdin()
+                    .read_line(&mut door)
+                    .expect("Failed to read line");
+                println!("Please choose blue, red, or yellow.");
+            }
+    
+        if door.trim_end() == "blue" {
+            println!("As you approach the door, the vegetation you are walking on sinks. You are in a marsh! You struggle to break free from the mud. Your efforts are in vain as the mud grips you from the waist down pulling you under with every muscle movement. You eventually are swallowed whole by the earth.");
+    } 
+    
+        else if door.trim_end() == "red" {
+            println!("You approach the door and you can feel an intense heat growing as you get closer. You open the door and lava spills forth, dissolving your body within seconds.");
+    } 
+    
+        else if door.trim_end() == "yellow" {
+            println!("You open the door with excitement for what lies beyond. As the door opens you gasp in disbelief, before you is a sea of gold for as far as the eye can see. You have found the treasure!");
+    }}}
+
+    // Pause before exit
+    println!("Game Over. Press Enter to exit.");
+    let stdin = std::io::stdin();
+    let _ = stdin.lock().lines().next();
+}
