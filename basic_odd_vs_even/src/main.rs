bd7b0af96bf66668148a6837e1f49966331f94cf
Joseph Abbott III <code.jabbott.iii@gmail.com>
2026-01-11 23:14:47 -0700

    finished program

--- basic_odd_vs_even/src/main.rs
+++ basic_odd_vs_even/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Odd or Even Check");
+    println!("Please enter a number: ");
+    // Get user input
+    let mut userdata = String::new();
+        std::io::stdin()
+            .read_line(&mut userdata)
+            .expect("Failed to read line");
+            
+        // Convert input to i64    
+        let userdata: i64 = userdata.trim()
+            .parse()
+            .expect("Please enter a valid number");
+
+    if userdata % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
+
+    // Pause before exit
+    println!("Press Enter to exit.");
+    let stdin = std::io::stdin();
+    let _ = stdin.lock().lines().next(); // Wait for user to press Enter
 }
